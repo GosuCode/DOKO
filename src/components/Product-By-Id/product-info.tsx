a825ed31cb29800dd21983eabe5a2ea1409c5b33
GosuCode <gosucode1945@gmail.com>
2024-09-24 23:10:32 +0545

    add: cosine coorelation

--- src/components/Product-By-Id/product-info.tsx
+++ src/components/Product-By-Id/product-info.tsx
@@ -13,7 +13,11 @@ function ProductInfo({ title, price, discount }: ProductInfoProps) {
         {title}
       </h1>
       <div className="text-xl text-palette-primary font-medium py-4 px-1">
-        <span className="line-through text-lg text-red-500">Rs.{price}</span>
+        <span
+          className={discount > 0 ? "line-through text-lg text-red-500" : ""}
+        >
+          Rs.{price}
+        </span>
         <br />
         <span className="text-purple-800">
           {discount > 0 ? <span>Rs.{discountedPrice.toFixed(0)}</span> : ""}
