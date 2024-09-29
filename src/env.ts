05fbda84bad1e3e1bdf6fe3074b88eed204e05e9
GosuCode <gosucode1945@gmail.com>
2024-09-29 18:32:47 +0545

    add: supabase

--- src/env.ts
+++ src/env.ts
@@ -5,10 +5,12 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
     NODE_ENV: z.string().min(1),
+    DB_CONNECTION_STRING: z.string().url()
   },
   client: {},
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
     DATABASE_URL: process.env.DATABASE_URL,
+    DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING
   },
 });
