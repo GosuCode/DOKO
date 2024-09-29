dc3d2c0e0360c9f3034e1d191ea626390ac100d1
GosuCode <gosucode1945@gmail.com>
2024-09-29 16:28:42 +0545

    fix: type error

--- src/app/product/order/page.tsx
+++ src/app/product/order/page.tsx
@@ -3,31 +3,32 @@ import OrderTable from "@/components/order/order-table";
 import PageTitle from "@/components/page-title";
 import BackToProductButton from "@/components/Product-By-Id/back-to-product-button";
 import React from "react";
-import { getOrder } from "./actions";
-import EmptyComponent from "@/components/empty-page";
+// import { getOrder } from "./actions";
+// import EmptyComponent from "@/components/empty-page";
 
 const OrderPage = async () => {
-  const order = await getOrder();
-  if (!order) {
-    return (
-      <>
-        <Header />
-        <EmptyComponent
-          pageName="order"
-          btnName="Add to Cart"
-          link="/product/product-list"
-          className="mt-24"
-        />
-      </>
-    );
-  }
+  // const order = await getOrder();
+  // if (!order) {
+  //   return (
+  //     <>
+  //       <Header />
+  //       <EmptyComponent
+  //         pageName="order"
+  //         btnName="Add to Cart"
+  //         link="/product/product-list"
+  //         className="mt-24"
+  //       />
+  //     </>
+  //   );
+  // }
 
   return (
     <>
       <Header />
       <div className="container mx-auto mt-20 mb-20">
         <PageTitle text="Your Order" />
-        <OrderTable order={order} />
+        {/* <OrderTable order={order} /> */}
+        <OrderTable />
         <div className="max-w-sm mx-auto space-y-4 px-2 flex flex-col space-x-2">
           <BackToProductButton />
         </div>
